bcc1c5216fe3cee798e897eed9fcaddbf4d16d76
磊磊 <839238345@qq.com>
2020-01-10 21:04:33 +0800

    增加获取资源方法

--- src/Infrastructure/src/EInfrastructure.Core.Tools/Component/IServiceProvider.cs
+++ src/Infrastructure/src/EInfrastructure.Core.Tools/Component/IServiceProvider.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) zhenlei520 All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace EInfrastructure.Core.Tools.Component
 {
@@ -17,6 +17,20 @@ namespace EInfrastructure.Core.Tools.Component
         /// <typeparam name="TService">服务类型</typeparam>
         /// <returns></returns>
         IEnumerable<TService> GetServices<TService>() where TService : class;
+
+        /// <summary>
+        /// 得到服务集合
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <typeparam name="TService"></typeparam>
+        /// <returns></returns>
+        IEnumerable<TService> GetServices<TService>(Assembly[] assemblies) where TService : class;
+
+        /// <summary>
+        /// 得到服务
+        /// </summary>
+        /// <returns></returns>
+        TService GetService<TService>(Assembly[] assemblies) where TService : class;
 
         /// <summary>
         /// 得到服务
--- src/Infrastructure/src/EInfrastructure.Core.Tools/Component/ServiceProvider.cs
+++ src/Infrastructure/src/EInfrastructure.Core.Tools/Component/ServiceProvider.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using EInfrastructure.Core.Tools.Systems;
 
 namespace EInfrastructure.Core.Tools.Component
@@ -28,13 +29,37 @@ namespace EInfrastructure.Core.Tools.Component
             {
                 yield return Activator.CreateInstance(type) as TService;
             }
+        }
 
-            // return list;
+        /// <summary>
+        /// 得到服务集合
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <typeparam name="TService"></typeparam>
+        /// <returns></returns>
+        public IEnumerable<TService> GetServices<TService>(Assembly[] assemblies) where TService : class
+        {
+            var types = assemblies.SelectMany(x =>
+                x.GetTypes().Where(y => y.GetInterfaces().Contains(typeof(TService)))).ToList();
+            foreach (var type in types)
+            {
+                yield return Activator.CreateInstance(type) as TService;
+            }
         }
 
         #endregion
 
         #region 得到服务
+
+        /// <summary>
+        /// 得到服务
+        /// </summary>
+        /// <returns></returns>
+        public TService GetService<TService>(Assembly[] assemblies) where TService : class
+        {
+            return GetServices<TService>(assemblies).FirstOrDefault();
+        }
+
 
         /// <summary>
         /// 得到服务
