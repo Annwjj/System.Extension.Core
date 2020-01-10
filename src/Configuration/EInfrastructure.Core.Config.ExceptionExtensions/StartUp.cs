03a9452cca1254151d8066dc7d3cba84c2a78676
磊磊 <839238345@qq.com>
2020-01-10 20:57:03 +0800

    更改目录结构

---
+++ src/Configuration/EInfrastructure.Core.Config.ExceptionExtensions/StartUp.cs
@@ -0,0 +1,24 @@
+// Copyright (c) zhenlei520 All rights reserved.
+
+namespace EInfrastructure.Core.Config.ExceptionExtensions
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class StartUp
+    {
+        private static bool _isStartUp;
+
+        /// <summary>
+        /// 启用配置
+        /// </summary>
+        public static void Run()
+        {
+            if (!_isStartUp)
+            {
+                _isStartUp = true;
+            }
+            EInfrastructure.Core.Config.EnumerationExtensions.StartUp.Run();
+        }
+    }
+}
